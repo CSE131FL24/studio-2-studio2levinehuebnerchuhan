0183cb4fa0413c5316fda51726b51bff1e2dd14b
Chuhan200 <chuhan.qiao@wustl.edu>
2024-09-11 15:50:44 -0500

    chuhan

--- src/studio2/Gambler.java
+++ src/studio2/Gambler.java
@@ -3,30 +3,36 @@ package studio2;
 public class Gambler {
 
 	public static void main(String[] args) {
-		int count=0;
-		int amount = 10;
-		for(int i =0;i<1000000;i++) {
-		var d = Math.random();
-		if (amount>20)
-		{
-			System.out.println("After "+count+" turns " +", You win");
-			System.exit(0);
-		}
-		else if (amount<0)
-		{
-			System.out.println("After "+count+" turns " +",You loose");
-			System.exit(0);
-		}
-		else {
+		for(int n =0;n<500;n++) {
+			int amount = 10;
+			int count=0;
+			int winround = 0;
+			int loseround =0 ;
+		while (amount<=20 && amount>=0) {
+			var d = Math.random();
 			if (d < 0.5) {
 				amount++;
 				count++;
+				winround ++;
 			}
 			else {
 				amount--;
 				count++;
+				loseround++;
 			}
 		}
+		
+		if (amount>20)
+		{
+			System.out.println("After "+count+" turns, "+"you won "+winround+" rounds, "+"you lost "+loseround+" rounds, " +",You win.");
+			
+		}
+		else if (amount<0)
+		{
+			System.out.println("After "+count+" turns, "+"you won "+winround+" rounds, "+"you lost "+loseround+" rounds, " +",You loose.");
+			
+		}
+	
 
 	}}
 
